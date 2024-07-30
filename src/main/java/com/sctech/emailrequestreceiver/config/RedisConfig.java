d3ef4448f8aab88bc7518b0bd49221db54efa16d
sctech <sctech>
2024-07-30 07:44:37 +0530

    redis config

--- src/main/java/com/sctech/emailrequestreceiver/config/RedisConfig.java
+++ src/main/java/com/sctech/emailrequestreceiver/config/RedisConfig.java
@@ -2,8 +2,11 @@ package com.sctech.emailrequestreceiver.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -12,9 +15,23 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfig = new RedisStandaloneConfiguration();
+        redisStandaloneConfig.setHostName(redisHost);
+        redisStandaloneConfig.setPort(redisPort);
+
+        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfigBuilder =
+                JedisClientConfiguration.builder();
+        JedisClientConfiguration jedisClientConfig = jedisClientConfigBuilder.build();
+
+        return new JedisConnectionFactory(redisStandaloneConfig, jedisClientConfig);
     }
 
     @Bean
